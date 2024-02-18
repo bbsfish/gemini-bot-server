0f6950baae0c3cc83d79c6a686532b8f65b76910
YSK <98343699+bbsfish@users.noreply.github.com>
2024-02-18 23:57:42 +0900

    update index

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const https = require('https');
 const router = express.Router();
 
 const gemini = require('../apis/gemini-ai.js');
@@ -10,23 +11,22 @@ router.get("/", (req, res) => {
 router.post('/', webhook);
 
 async function webhook(req, res) {
-  const data = req.body; // <= {from: '***', subject: '', body: ''}
-  console.log(data);
+  const body = req.body; // <= {from: '***', subject: '', body: ''}
+  console.log(body);
   
-  const response = await fetch(process.env.LINE_BOT_URL, {
+  const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    payload: JSON.stringify(data)
-  });
-
-  if (response.ok) {
-    // const result = await res.json();
-    console.log('ok!');
-  } else {
-    console.log('error!');
   }
+  const data = JSON.stringify({
+    content: body
+  });
+  
+  const request = https.request(url, options);
+  request.write(data);
+  request.end();
 
 
   res.end();
